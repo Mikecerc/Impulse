8cfc2fb8aee97cd2548e676f89a32fbf07629a86
Mikecerc1 <mikecerc1@outlook.com>
2022-08-17 23:08:34 -0400

    ts conversion, music updates, command updates, bug fixes, general parody of illusion

---
+++ src/schemas/pollSchema.js
@@ -0,0 +1,24 @@
+import pkg from "mongoose";
+const { Schema, model } = pkg;
+//create mongoose schema (class)
+const pollSchema = new Schema({
+    messageId: String,
+    anonymous: Boolean,
+    multipleResponse: Boolean,
+    creator: String,
+    serverId: String,
+    content: {
+        question: String,
+        answers: Array,
+    },
+    responses: [
+        {
+            userId: String,
+            answers: Array,
+        },
+    ],
+});
+//export schema as a mongoose model
+const pollModel = model("pollData", pollSchema, "pollData");
+
+export default pollModel;
