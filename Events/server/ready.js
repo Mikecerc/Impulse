085221e3fa3a775001906e99a35689f62892a529
Mikecerc1 <mikecerc1@outlook.com>
2022-08-17 23:09:12 -0400

    ts conversion, music updates, command updates, bug fixes, general parody of illusion

--- Events/server/ready.js
+++
@@ -1,7 +0,0 @@
-export default {
-    name: 'ready',
-    execute(client) {
-        console.log('The client is ready :)');
-        client.user.setActivity('Shane Hurley', { type: 'WATCHING' });
-    },
-};
